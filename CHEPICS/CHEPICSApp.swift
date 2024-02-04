855ce5f4db91843fd28994f845388040cc021a37
川尻辰義 <grabdepa@gmail.com>
2024-02-04 15:41:12 +0900

    Initial Commit

---
+++ CHEPICS/CHEPICSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHEPICSApp.swift
+//  CHEPICS
+//
+//  Created by 川尻辰義 on 2024/02/04.
+//
+
+import SwiftUI
+
+@main
+struct CHEPICSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CHEPICS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CHEPICS
+//
+//  Created by 川尻辰義 on 2024/02/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
