d739ff679c633d428c3a24f59cee1b546496783e
tatsuyoshi-kawajiri <tatsuyoshi-kawajiri@navitime.co.jp>
2024-02-04 15:57:58 +0900

    FeedView作成

---
+++ CHEPICS/Feature/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  CHEPICS
+//
+//  Created by tatsuyoshi-kawajiri on 2024/02/04.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
