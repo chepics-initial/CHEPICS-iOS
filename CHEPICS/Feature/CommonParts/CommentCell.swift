6d5432185fb10e8c804332c89655170502339a84
tatsuyoshi-kawajiri <tatsuyoshi-kawajiri@navitime.co.jp>
2024-04-01 08:59:17 +0900

    共通化パーツの配置修正

---
+++ CHEPICS/Feature/CommonParts/CommentCell.swift
@@ -0,0 +1,18 @@
+//
+//  CommentCell.swift
+//  CHEPICS
+//
+//  Created by tatsuyoshi-kawajiri on 2024/04/01.
+//
+
+import SwiftUI
+
+struct CommentCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentCell()
+}
