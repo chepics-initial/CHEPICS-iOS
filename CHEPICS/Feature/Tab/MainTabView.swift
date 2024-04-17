403605ea37202b55b1b9ad3fb46050cab4518d86
川尻辰義 <kawajiritatsuyoshi@kawajiroshisMBP.lan>
2024-04-17 12:57:42 +0900

    tabの色修正のため全体的に設計修正

--- CHEPICS/Feature/Tab/MainTabView.swift
+++ CHEPICS/Feature/Tab/MainTabView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftUIIntrospect
 
 struct MainTabView: View {
     @StateObject private var viewModel = MainTabViewModel()
@@ -14,24 +15,31 @@ struct MainTabView: View {
     @State private var myPageStack: NavigationPath = .init()
     
     var body: some View {
-        TabView(selection: tabSelection) {
-            NavigationStack(path: $feedStack) {
-                FeedView(viewModel: FeedViewModel(feedUseCase: DIFactory.feedUseCase()))
-                    .environmentObject(viewModel)
+        VStack(spacing: 0) {
+            TabView(selection: tabSelection) {
+                NavigationStack(path: $feedStack) {
+                    FeedView(viewModel: FeedViewModel(feedUseCase: DIFactory.feedUseCase()))
+                        .environmentObject(viewModel)
+                }
+                .tag(Tab.feed)
+                .tabItem {
+                    Image(systemName: "house")
+                }
+                
+                NavigationStack(path: $myPageStack) {
+                    ProfileView(viewModel: ProfileViewModel(profileUseCase: DIFactory.profileUseCase()))
+                        .environmentObject(viewModel)
+                }
+                .tag(Tab.myPage)
+                .tabItem {
+                    Image(systemName: "person")
+                }
             }
-            .tag(Tab.feed)
-            .tabItem {
-                Image(systemName: "house")
+            .introspect(.tabView, on: .iOS(.v16, .v17)) { tabView in
+                tabView.tabBar.isHidden = true
             }
             
-            NavigationStack(path: $myPageStack) {
-                ProfileView(viewModel: ProfileViewModel(profileUseCase: DIFactory.profileUseCase()))
-                    .environmentObject(viewModel)
-            }
-            .tag(Tab.myPage)
-            .tabItem {
-                Image(systemName: "person")
-            }
+            CustomTabBar()
         }
         .overlay {
             if viewModel.showImageViewer {
@@ -65,7 +73,37 @@ struct MainTabView: View {
             
             activeTab = newValue
         }
+    }
+    
+    @ViewBuilder
+    private func CustomTabBar() -> some View {
+        HStack(spacing: 0) {
+            ForEach(Tab.allCases, id: \.self) {
+                TabItem(
+                    tab: $0,
+                    activeTab: tabSelection)
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+    }
+}
 
+struct TabItem: View {
+    var tab: Tab
+    @Binding var activeTab: Tab
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: activeTab == tab ? tab.selectedImage : tab.unselectedImage)
+                .font(.title2)
+                .foregroundStyle(activeTab == tab ? .chepicsPrimary : .gray.opacity(0.4))
+                .frame(width: 40, height: 40)
+        }
+        .frame(maxWidth: .infinity)
+        .onTapGesture {
+            activeTab = tab
+        }
     }
 }
 
@@ -73,7 +111,25 @@ struct MainTabView: View {
     MainTabView()
 }
 
-enum Tab {
+enum Tab: CaseIterable {
     case feed
     case myPage
+    
+    var selectedImage: String {
+        switch self {
+        case .feed:
+            "house.fill"
+        case .myPage:
+            "person.fill"
+        }
+    }
+    
+    var unselectedImage: String {
+        switch self {
+        case .feed:
+            "house"
+        case .myPage:
+            "person"
+        }
+    }
 }
