0bb3610801c75ab66ca696bc42ab09dcecb9b17a
tatsuyoshi-kawajiri <tatsuyoshi-kawajiri@navitime.co.jp>
2024-04-22 13:58:50 +0900

    検索実行処理を追加

--- CHEPICS/Feature/Explore/Search/ExploreTopView.swift
+++ CHEPICS/Feature/Explore/Search/ExploreTopView.swift
@@ -10,8 +10,10 @@ import SwiftUIIntrospect
 
 struct ExploreTopView: View {
     @Environment(\.dismiss) var dismiss
+    @Environment(\.colorScheme) var colorScheme
     @FocusState private var isFocused: Bool
     @StateObject var viewModel: ExploreTopViewModel
+    @State private var isPresented = false
     
     var body: some View {
         VStack {
@@ -36,6 +38,9 @@ struct ExploreTopView: View {
                     TextField("検索", text: $viewModel.searchText)
                         .focused($isFocused)
                         .submitLabel(.search)
+                        .onSubmit {
+                            isPresented = true
+                        }
                         .frame(maxWidth: .infinity)
                         .introspect(.textField, on: .iOS(.v16, .v17
                                                         )) { textField in
@@ -64,23 +69,30 @@ struct ExploreTopView: View {
             
             if !viewModel.searchText.isEmpty {
                 VStack {
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 16, height: 16)
-                            .foregroundStyle(.chepicsPrimary)
-                        
-                        Text(viewModel.searchText)
-                            .fontWeight(.semibold)
-                            .lineLimit(1)
-                        
-                        Text("を検索")
-                        
-                        Spacer()
+                    Button {
+                        isPresented = true
+                    } label: {
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 16, height: 16)
+                                .foregroundStyle(.chepicsPrimary)
+                            
+                            Text(viewModel.searchText)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .foregroundStyle(Color.getDefaultColor(for: colorScheme))
+                            
+                            Text("を検索")
+                                .foregroundStyle(Color.getDefaultColor(for: colorScheme))
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal, 16)
+                        .padding(.top, 16)
                     }
-                    .padding(.horizontal, 16)
-                    .padding(.top, 16)
+
                     
                     Divider()
                 }
@@ -91,6 +103,9 @@ struct ExploreTopView: View {
         .navigationBarBackButtonHidden()
         .onAppear {
             isFocused = true
+        }
+        .navigationDestination(isPresented: $isPresented) {
+            ExploreResultView()
         }
     }
 }
