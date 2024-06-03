ac33f2f243b6eaa3f7c947bed4786ff77fa171d3
tatsuyoshi-kawajiri <tatsuyoshi-kawajiri@navitime.co.jp>
2024-06-03 17:52:48 +0900

    セットの配列取得時にoffsetを指定できるよう修正

--- CHEPICS/Repository/DataSource/SetDataSource.swift
+++ CHEPICS/Repository/DataSource/SetDataSource.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol SetDataSource {
-    func fetchSets(topicId: String) async -> Result<[PickSet], APIError>
+    func fetchSets(topicId: String, offset: Int?) async -> Result<[PickSet], APIError>
     func createSet(body: CreateSetBody) async -> Result<Void, APIError>
     func pickSet(body: PickSetBody) async -> Result<PickSet, APIError>
     func fetchSet(setId: String) async -> Result<PickSet, APIError>
